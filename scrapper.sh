abe8d41bc1fc180af7c4e3b690d80f550c809458
Ubuntu <ubuntu@ip-172-31-56-198.ec2.internal>
2023-03-26 22:18:45 +0000

    Final push for the project

--- scrapper.sh
+++ scrapper.sh
@@ -14,3 +14,11 @@ echo "$time Bitcoin price: $price"
 
 # Save the result to a file
 echo "$time Bitcoin price: $price" >> price.txt
+
+#Kill all the processus
+sudo fuser -k 8050/tcp
+
+#update the python code 
+/usr/bin/python3 /home/ubuntu/Linux-Saison8/getPrice.py
+
+/usr/bin/python3 /home/ubuntu/Linux-Saison8/fileClear.py
