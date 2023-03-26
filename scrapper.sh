9ecf5757f258d214929a35573f124add14c1da1b
StanislasBANCELSchool <128978393+StanislasBANCELSchool@users.noreply.github.com>
2023-03-27 01:27:47 +0200

    Update scrapper.sh

--- scrapper.sh
+++ scrapper.sh
@@ -18,8 +18,8 @@ echo "$time Bitcoin price: $price"
 # Save the result to a file
 echo "$time Bitcoin price: $price" >> price.txt
 
-#Kill all the processus
+# Kill all the processus
 sudo fuser -k 8050/tcp
 
-#update the python code 
+# Update the python code 
 /usr/bin/python3 /home/ubuntu/Linux-Saison8/getPrice.py
